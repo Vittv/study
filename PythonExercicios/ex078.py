0dd11d7f5b5ee379911aff5c593f8e15763668b1
Vittv <vittoriobrum@hotmail.com>
2024-04-11 11:59:00 -0300

    #

--- PythonExercicios/ex078.py
+++ PythonExercicios/ex078.py
@@ -1,17 +1,28 @@
 # Faça um programa que leia 5 valores, colocando-os em uma lista
 # e mostrando qual o maior e menor e suas respectivas posições:
 
-lista = list()
-
+list = []
+max = 0
+min = 0
 for c in range(0, 5):
-    lista.append(int(input('Digite um valor: ')))
+    list.append(int(input('Enter a value: ')))
+    if c == 0:
+        max = min = list[c]
+    else:
+        if list[c] > max:
+            max = list[c]
+        if list[c] < min:
+            min = list[c]
 
-maxvalor = max(lista)
-minvalor = min(lista)
+print(f'You entered {list}')
+print(f'Max was {max}, found at: ', end='')
+for i, v in enumerate(list):
+    if v == max:
+        print(f'{i+1}..', end='')
+print()
 
-maxpos = [i + 1 for i, v in enumerate(lista) if v == maxvalor]
-minpos = [i + 1 for i, v in enumerate(lista) if v == minvalor]
-
-print(f'Você digitou {lista}')
-print(f'O maior valor foi {maxvalor} encontrado nas posições {maxpos}')
-print(f'O menor valor foi {minvalor} encontrado nas posições {minpos}')
+print(f' Min was {min}, found at: ', end='')
+for i, v in enumerate(list):
+    if v == min:
+        print(f'{i+1}..', end='')
+print()
