94fcc2e841b0ac53db067fe4ddb0393bb87faf90
Vittv <vittoriobrum@hotmail.com>
2024-04-19 16:00:46 -0300

    #

--- Python/cursoemvideo/pythontests/aula20a.py
+++ Python/cursoemvideo/pythontests/aula20a.py
@@ -33,7 +33,7 @@ def mensagem(msg):
     print(msg)
     print('-----------------------------')
 
-
+ 
 mensagem('SISTEMA DE ALUNOS')
 
 
@@ -47,3 +47,44 @@ def título(txt):
 título('    CURSO EM VÍDEO    ')
 título('    APRENDA PYTHON    ')
 título('    GUSTAVO GUANABARA    ')
+
+
+def soma(a, b):
+    print(f'A = {a} e B = {b}')
+    s = a + b
+    print(f'A soma A + B = {s}')
+
+
+# Programa Principal
+soma(a = 4, b = 5)  # É possível mudar a ordem contanto que seja explícito
+soma(8, 9)
+soma(2, 1)
+
+def contador(*num):
+    tam = len(num)
+    print(f'Recebi os valores {num} e são ao todo {tam} números.')
+
+contador(2, 1, 7)
+contador(8, 0)
+contador(4, 4, 7, 6, 2)
+
+def dobra(lst):
+    pos = 0
+    while pos < len(lst):
+        lst[pos] *= 2
+        pos += 1 
+
+
+valores = [7, 2, 5, 0, 4]
+dobra(valores)
+print(valores)
+
+def some(* valores):
+    s = 0
+    for num in valores:
+        s += num
+    print(f'Somando os valores {valores} temos {s}.')
+
+
+some(5, 2)
+some(2, 9, 4)
