587b4480cc4f7ea004d4c4d33891fbdc66bd505f
Vittv <vittoriobrum@hotmail.com>
2024-04-19 15:20:03 -0300

    #

---
+++ Python/cursoemvideo/pythontests/aula20a.py
@@ -0,0 +1,49 @@
+def mostralinha():
+
+    print('----------------------')
+
+mostralinha()
+print('       SISTEMA DE ALUNOS       ')
+mostralinha()
+mostralinha()
+print('    CADASTRO DE FUNCIONÁRIOS   ')
+mostralinha()
+mostralinha()
+print('        ERRO DO SISTEMA        ')
+mostralinha()
+
+def lin():
+    print('-' * 30)
+
+
+# Programa Principal:
+lin()
+print('   CURSO EM VÍDEO   ')
+lin()
+lin()
+print('   APRENDA PYTHON   ')
+lin()
+lin()
+print('  GUSTAVO GUANABARA ')
+lin()
+
+
+def mensagem(msg):
+    print('-----------------------------')
+    print(msg)
+    print('-----------------------------')
+
+
+mensagem('SISTEMA DE ALUNOS')
+
+
+def título(txt):
+    print('-' * len(txt))
+    print(txt)
+    print('-' * len(txt))
+
+
+# Programa Principal
+título('    CURSO EM VÍDEO    ')
+título('    APRENDA PYTHON    ')
+título('    GUSTAVO GUANABARA    ')
