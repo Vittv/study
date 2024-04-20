9857c174f9df879b21471754a11dec5c899f3cde
Vittv <vittoriobrum@hotmail.com>
2024-04-20 19:01:49 -0300

    #

--- Python/cursoemvideo/pythonex/ex100.py
+++ Python/cursoemvideo/pythonex/ex100.py
@@ -7,13 +7,18 @@ números = []
 def sorteia():
 
     print('Sorteando 5 valores da lista: ', end='')
+    
     for i in range(5):
+
         valores = randint(1, 10)
         números.append(valores)
+    
     for núm in números:
+        
         print(f'{núm} ', end='', flush=True)
         sys.stdout.flush()
         sleep(0.5)
+    
     print('PRONTO!')
 
 
@@ -25,6 +30,7 @@ def somaPar():
 
         if valor % 2 == 0:
             totalpar += valor
+    
     print(f'Somando os valores pares de {números}, temos {totalpar}')
 
 
