8f1ff1bb54a42ed2e68e723dd7c0d22fe8ef4df0
Vittv <vittoriobrum@hotmail.com>
2024-04-21 13:56:39 -0300

    #

--- Python/pythonprojects/vitt.py
+++ Python/pythonprojects/vitt.py
@@ -1,20 +1,71 @@
+import discord
+from discord.ext import commands
 from random import randint
 from datetime import datetime
 
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+bot = commands.Bot(command_prefix=">", intents=intents)
 
-def roll():
+@bot.command()
+async def hello(ctx):
+    print('Hello!')
+
+
+@bot.command()
+async def punch(ctx, arg):
+
+    # >punch Whoever
+
+    await ctx.send(f'Punched {arg}')
+
+
+@bot.command()
+async def double_punch(ctx, arg1, arg2):
+
+    # >punch Whoever
+
+    await ctx.send(f'Double punched {arg1} and {arg2}')
+
+
+@bot.command()
+async def info(ctx):
+
+    # ctx = context (information about how the command was executed)
+
+    # >info
+
+    await ctx.send(ctx.guild)
+    await ctx.send(ctx.author)
+    await ctx.send(ctx.message.id)
+
+@bot.command()
+async def roll(ctx):
+
     roll = randint(1, 100)
-    print(f'User rolled {roll}!')
+    print(f'@user rolled {roll}!')
 
 
 birthdays = []
 
-def birthday():
-    birthday = str(input('Please, type in your birthday! '))
-    birthdays.append(birthday)
+@bot.command()
+async def get_birthday(ctx):
 
-    if birthday == datetime.now():
-        print(f'Happy Birthday! @user >w<
-              \nHope you have an awesome day!')
-        
+    birthday = input('Please, type in your birthday (YYYY-MM-DD): ')
+    try:
+        birthday_date = datetime.strptime(birthday, "%Y-%m-%d").date()
+        birthdays.append(birthday_date)
+        return birthday_date
+    
+    except ValueError:
+        print("Invalid date format. Please enter your birthday in")
 
+
+def check_birthdays_today():
+
+    today = datetime.now().date()
+    for birthday in birthdays:
+
+        if birthday.month == today.month and birthday.day == today.day:
+            print(f"Happy Birthday, @user! >w<\nHope you have a great day!")
