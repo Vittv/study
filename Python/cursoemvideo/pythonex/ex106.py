e4017bf5f1ed2fa7a7836fa0fdf63765f75ff12b
Vittv <vittoriobrum@hotmail.com>
2024-04-25 01:37:19 -0300

    #

--- Python/cursoemvideo/pythonex/ex106.py
+++ Python/cursoemvideo/pythonex/ex106.py
@@ -1,30 +1,36 @@
 from time import sleep
 import sys
 
+GREEN = '\033[42m'
+BLUE = '\033[44m'
+NEGATIVE = '\033[7m'
+RESET = '\033[0m' 
+
+def colored_print(text, color):
+    print(color + text + RESET)
+
 def PyHelp():
     while True:
 
-        print('~' * len('  SISTEMA DE AJUDA PyHELP  '))
-        print('  SISTEMA DE AJUDA PYHELP  ')
-        print('~' * len('  SISTEMA DE AJUDA PyHELP  '))
-        sys.stdout.flush()
+        colored_print('~' * len('  SISTEMA DE AJUDA PyHELP  '), GREEN)
+        colored_print('  SISTEMA DE AJUDA PYHELP  ')
+        colored_print('~' * len('  SISTEMA DE AJUDA PyHELP  '), GREEN)
 
         prompt = input('Função ou Biblioteca > ')
-        sys.stdout.flush()
 
-        print('~' * len(f"Acessando o manual do comando '{prompt}' "))
-        print(f"Acessando o manual do comando '{prompt}' ")
-        print('~' * len(f"Acessando o manual do comando '{prompt}' "))
-        sys.stdout.flush()
+        sleep(1)
 
-        sleep(0.5)
+        colored_print('~' * len(f"Acessando o manual do comando '{prompt}' "), BLUE)
+        colored_print(f"Acessando o manual do comando '{prompt}' ")
+        colored_print('~' * len(f"Acessando o manual do comando '{prompt}' "), BLUE)
+
+        sleep(1)
 
         if prompt.lower() == 'fim':
             print('Encerrando o sistema de ajuda.')
             break
            
-        print(help(prompt))
-        sys.stdout.flush()
+        colored_print(help(prompt), NEGATIVE)
 
 
 PyHelp()
